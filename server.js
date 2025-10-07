90968d253babd1250107d6e050163246f94d7325
TechWarrior12 <web.development@tritechnologies.net>
2025-10-08 03:14:49 +0500

    Dir

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ import {
   findExistingPrivateChat,
   createPrivateConversation,
   createGroupConversation
-} from './src/utils/db-operations.js';
+} from './src/app/utils/db-operations.js';
 
 // Load environment variables
 dotenv.config();
